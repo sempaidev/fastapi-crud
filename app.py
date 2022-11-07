40f62fc9bab97fb697e907dd64c4a2bbc98fd70d
ealvarado <ealvarado@titan.com.pa>
2022-11-07 16:31:31 -0500

    add method crud

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from model import Post
 
 app = FastAPI()
 
@@ -13,6 +14,77 @@ def read_root():
     """
     return {"bienvenido": "Bienvenidos a mi api"}
 
-@app.get('/post')
-def posts():
+@app.get('/posts')
+def get_posts():
+    """
+    Devuelve la variable de publicaciones.
+    :return: Una lista de diccionarios.
+    """
     return posts
+
+@app.post('/posts')
+def save_post(post: Post):
+    """
+    Toma un objeto Publicar, lo convierte en un diccionario y lo agrega a la lista de publicaciones.
+    
+    :param post: Correo
+    :type post: Post
+    :return: El último elemento de la lista.
+    """
+    posts.append(post.dict())
+    return posts[-1]
+
+
+@app.get('/posts/{id}')
+def get_post(id: str):
+    """
+    Si la identificación de la publicación es igual a la identificación pasada, devolver la publicación
+    
+    :param id: id de la publicación
+    :type id: str
+    :return: Una publicación
+    """
+    for post in posts:
+        print(post)
+        if str(post['id']) == id:
+            return post
+    
+    raise HTTPException(status_code=404, detail="Post no encontrado")
+
+@app.delete('posts/{id}')
+def update_post(id: str):
+    """
+    Si la identificación de la publicación coincide con la identificación de la publicación en la lista,
+    actualice el título, el contenido y el autor de la publicación.
+    
+    :param id: calle
+    :type id: str
+    :return: Un diccionario con un mensaje.
+    """
+    for index, post in enumerate(posts):
+        if str(post['id']) == id:
+            posts[index]["title"] = data.title
+            posts[index]["content"] = data.content
+            posts[index]["author"] = data.author
+
+            return {"message": "Post actualizado correctamente"}
+
+    raise HTTPException(status_code=404, detail="Post no encontrado")
+
+
+@app.delete('posts/{id}')
+def delete_post(id: str):
+    """
+    Recorre la lista de publicaciones y, si la identificación de la publicación coincide con la
+    identificación pasada, elimina la publicación de la lista y devuelve un mensaje.
+    
+    :param id: calle
+    :type id: str
+    :return: Un diccionario con un mensaje.
+    """
+    for index, post in enumerate(posts):
+        if str(post['id']) == id:
+            posts.pop(index)
+            return {"message": "Post eliminado correctamente"}
+
+    raise HTTPException(status_code=404, detail="Post no encontrado")
